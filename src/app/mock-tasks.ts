7c2663f6f8b7b331da3bdb85c1e36be96eb880f0
StrombergT <tpr.stromberg@gmail.com>
2023-10-19 14:48:39 +0200

    added tasks

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,24 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Lämna bilen på service',
+    day: '20:e September klockan 15:00',
+    reminder: true,
+  },
+
+  {
+    id: 2,
+    text: 'Möte på skolan',
+    day: '25:e September klockan 17:00',
+    reminder: true,
+  },
+
+  {
+    id: 3,
+    text: 'Tid hos tandläkaren',
+    day: '27:e September klockan 10:00',
+    reminder: true,
+  },
+];
